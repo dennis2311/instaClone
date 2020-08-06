6f543a715351ca0bf9a1b80534752dad774c50bf
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-08-06 17:41:25 +0900

    ~#3.4

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-require("dotenv").config();
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import utils from "./utils";
+import { sendSecretMail } from "./utils";
+
+// This can't be until verifying sendgrid mailer
+// sendSecretMail("dennis2311@daum.net", "111223");
 
 const PORT = process.env.PORT || 4000;
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = async (email) => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD,
+    },
+  };
+  const client = await nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "instaCloneAdmin@admin.com",
+    to: address,
+    subject: "Your Secret Words Ready",
+    html: `<br>Hello! your login secret word is ${secret}<br>`,
+  };
+  return sendMail(email);
 };
