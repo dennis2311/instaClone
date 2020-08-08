1262a03b4f72f214f0ce31b2ae4e87bd413fba2d
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-08-08 16:40:24 +0900

    ~#3.17 (seeUser Crash)

---
+++ src/api/User/computed.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent; //it takes id in parent and change its name into parentId
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: parentId }, { following_some: { id: user.id } }],
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    },
+  },
+};
