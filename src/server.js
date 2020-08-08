71abfe5733005fd7fd7850b8e61f2cbae356ad1c
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-08-08 12:52:52 +0900

    ~#3.12

--- src/server.js
+++ src/server.js
@@ -12,12 +12,13 @@ import passport from "passport";
 import utils from "./utils";
 import { sendSecretMail } from "./utils";
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request }),
+  context: ({ request }) => ({ request, isAuthenticated }),
 });
 
 server.express.use(logger("dev"));
