86a3251ad0050c4d2031fb8b1eedb6639006148d
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-08-20 21:13:45 +0900

    ~#5.6

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,18 +4,18 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      try {
-        await prisma.createUser({
-          username,
-          email,
-          firstName,
-          lastName,
-          bio,
-        });
-        return true;
-      } catch {
-        return false;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username is already taken");
       }
+      await prisma.createUser({
+        username,
+        email,
+        firstName,
+        lastName,
+        bio,
+      });
+      return true;
     },
   },
 };
